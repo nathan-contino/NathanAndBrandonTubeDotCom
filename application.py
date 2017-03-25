dd937d0206ece66c103f4dd353f80cb7ea53f8ac
Brandon Willett <bcwillett1@gmail.com>
2017-03-25 19:50:29 -0400

    added all functionality

--- application.py
+++ application.py
@@ -7,6 +7,23 @@ application = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@application.route('/login', methods = ['GET'])
+def login():
+
+	userList = open("username.txt").readlines()
+	uname = request.args.get('username')
+	pword = request.args.get('password')
+
+	for user in userList:
+		infos = user.rstrip('\n').split(" ")
+		print(infos)
+		if (infos[0] == uname):
+			if (infos[1] == pword):
+				return "success"
+			else: return "failure"
+
+	return "no such user"
+
 if __name__ == "__main__":
     application.debug = True
     application.run(port = 8080)
